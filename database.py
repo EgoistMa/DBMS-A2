4fc1bb69eb8917c18d97c2c5db982a85aaf59a23
EGOIST <tianyuma4869@gmail.com>
2023-10-16 00:15:07 +1100

    add,find

--- database.py
+++ database.py
@@ -10,8 +10,8 @@ Connect to the database using the connection string
 '''
 def openConnection():
     # connection parameters - ENTER YOUR LOGIN AND PASSWORD HERE
-    #userid = "y23s2c9120_unikey"
-    #passwd = ""
+    #userid = "y23s2c9120_tima3723"
+    #passwd = "af7HKxEh"
     #myHost = "soit-db-pro-2.ucc.usyd.edu.au"
 
     userid = "postgres"
@@ -45,6 +45,21 @@ def checkEmployeeCredentials(userName, password):
     return result
     #return ['jswift', '111', 'James', 'Swift', '0422834091', 'jamesswift@hotmail.com', '7 Tesolin Way Westgate QLD']
 
+def getCarsFromResult(result):
+    cars = []
+    for row in result:
+        purchase_date = row[4]
+        formatted_date = purchase_date.strftime('%d/%m/%Y')# todo check if we need do a date formating or not? & is that ok to do a software level formating?
+        car = {
+            "car_id": row[0],
+            "makemodel": row[1],  
+            "status": row[2],
+            "typewheel": row[3],
+            "purchasedate": formatted_date,  
+            "employee": row[5],
+            "description": row[6]  
+        }
+    cars.append(car)
 
 '''
 List all the associated cars in the database by employee
@@ -52,32 +67,86 @@ List all the associated cars in the database by employee
 def findCarsByEmployee(userName):
     conn = openConnection()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM car C WHERE C.managedby = %s", (userName,))
+    cur.execute("SELECT * FROM CarDetails WHERE \"Managed By\" = %s or \"Managed By\" is null order by purchasedate asc,description asc ,status desc", (userName,))
     result = cur.fetchall()
     cur.close()
     conn.close()
     print(result)
-    return [(6,'kia',1,2,'10/10/2023','jswift','desc')]
-    #return [(6, 'Kia', 'Sportage', 2, 2, 3, '10/10/2023', 'jswift', 'Fusing a long, extremely athletic body with an unstoppable attitude, the redesigned Sportage is the new benchmark medium SUV.')]
-    #return [(6,6,6,6,6,6,6)]
-    return result
+    cars = getCarsFromResult(result)
+
+    return cars
 
 
 '''
 Find a list of cars based on the searchString provided as parameter
 See assignment description for search specification
 '''
-def findCarsByCriteria(searchString):
+def findCarsByCriteria(searchString): #added username for better search 
+    conn = openConnection()
+    cur = conn.cursor()
 
-    return
+    query = """
+            select
+            * 
+            from 
+            cardetails c 
+            where 
+            c.id in (
+            SELECT
+                car.carid 
+                FROM Car car
+                LEFT JOIN Status s ON car.statusid = s.statusid
+                left join cartype ct on car.cartypeid = ct.cartypeid 
+                left join carwheel cw on car.carwheelid = cw.carwheelid 
+                left join employee e on car.managedby = e.username
+                WHERE 
+                    (
+                        UPPER(make) LIKE UPPER(%s) OR
+                        UPPER(model) LIKE UPPER(%s) OR
+                        UPPER(s.statusName) LIKE UPPER(%s) OR
+                        UPPER(ct.carTypeName) LIKE UPPER(%s) OR
+                        UPPER(cw.carwheelname) LIKE UPPER(%s) OR
+                        UPPER(CONCAT(e.firstname, ' ', e.lastname)) LIKE UPPER(%s) OR
+                        UPPER(description) LIKE UPPER(%s)
+                    )
+                    AND car.purchaseDate >= CURRENT_DATE - INTERVAL '15 years'
+            )
+            ORDER BY
+                c."Managed By" IS NULL DESC,
+                c.purchaseDate ASC
+            ;
+            """
+    search_pattern = "%" + searchString + "%"  # create a pattern for the LIKE query
+    cur.execute(query, (search_pattern, search_pattern, search_pattern, search_pattern, search_pattern, search_pattern, search_pattern))
+    result = cur.fetchall()
+    
+    cur.close()
+    conn.close()
+
+    print(result)
+    cars = getCarsFromResult(result)
+    return cars
 
 
 '''
 Add a new car
 '''
 def addCar(make, model, type, wheel, purchasedate, description):
+    conn = openConnection()
+    cur = conn.cursor()
 
-    return
+    query = """
+            select insert_car(%s,%s,%s,%s,%s,%s);
+            """
+    
+    cur.execute(query, (make, model, type, wheel, purchasedate, description))
+    result = cur.fetchone()
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return result[0] == 'Success'
 
 
 '''
