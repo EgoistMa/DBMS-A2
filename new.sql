4bb0bf36e991f8003b3624360bdefd07be98cb95
EGOIST <tianyuma4869@gmail.com>
2023-10-17 09:56:15 +1100

    added new sqls that used in project

--- new.sql
+++ new.sql
@@ -1,6 +1,5 @@
 
-drop view if exists CarDetails;
-CREATE VIEW CarDetails AS
+CREATE OR REPLACE VIEW CarDetails AS
 SELECT
     c.carID AS ID,
     CONCAT(c.make, ' ', c.model) AS "Make & Model",
@@ -17,18 +16,18 @@ LEFT JOIN Employee e ON c.managedBy = e.userName;
 
 
 CREATE OR REPLACE FUNCTION insert_car(
-    p_make VARCHAR, 
-    p_model VARCHAR, 
-    p_type VARCHAR, 
-    p_wheel VARCHAR, 
+    p_make VARCHAR(30), 
+    p_model VARCHAR(30), 
+    p_type VARCHAR(10), 
+    p_wheel VARCHAR(10), 
     p_purchasedate DATE, 
-    p_description TEXT
+    p_description VARCHAR(400)
 ) RETURNS text AS $$
 DECLARE
     v_cartypeid INT;
     v_carwheelid INT;
    	v_statusid INT;
-    v_error_message TEXT;
+    v_error_message VARCHAR(400);
 BEGIN
     -- 检查carType 是否合法，如果不合法抛出异常
     SELECT cartypeid INTO v_cartypeid 
@@ -52,7 +51,7 @@ BEGIN
    	SELECT statusID INTO v_statusid 
     FROM Status 
     WHERE statusName = 'New Stock';
-    -- 使用得到的cartypeid和carwheelid更新car表
+    -- 更新car表
    INSERT INTO Car (make, model, statusid, cartypeid, carwheelid, purchasedate, managedby, description)
     VALUES(p_make, p_model,v_statusid, v_cartypeid, v_carwheelid, p_purchasedate,null, p_description);
 
@@ -97,5 +96,77 @@ BEGIN
         ORDER BY
             c."Managed By" IS NULL DESC,
             c.PurchaseDate ASC;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION update_car(
+    p_carID INT,
+    p_make VARCHAR(30),
+    p_model VARCHAR(30),
+    p_status VARCHAR(20),
+    p_type VARCHAR(10),
+    p_wheel VARCHAR(10),
+    p_purchaseDate DATE,
+    p_employee VARCHAR(20),
+    p_description VARCHAR(400)
+)
+RETURNS VARCHAR AS $$
+DECLARE
+    v_statusID INT;
+    v_typeID INT;
+    v_wheelID INT := NULL;
+    v_employee VARCHAR(20) := NULL;
+   	v_description VARCHAR(400) := NULL;
+BEGIN
+
+    -- 检查statusName 是否合法，如果不合法抛出异常
+    SELECT statusID INTO v_statusID FROM Status WHERE statusName = p_status;
+    IF v_statusID IS NULL THEN
+        RETURN 'Invalid status provided: ' || p_status;
+    END IF;
+
+    -- 检查carTypeName 是否合法，如果不合法抛出异常
+    SELECT carTypeID INTO v_typeID FROM CarType WHERE carTypeName = p_type;
+    IF v_typeID IS NULL THEN
+        RETURN 'Invalid type provided: ' || p_type;
+    END IF;
+   
+   -- 检查carWheelName是否不为空 且 是否合法，如果空则为null 不合法抛出异常
+   	IF p_wheel IS NOT NULL AND p_wheel <> '' THEN
+        SELECT carWheelID INTO v_wheelID FROM CarWheel WHERE carWheelName = p_wheel;
+        IF v_wheelID IS NULL THEN
+            RETURN 'Invalid wheel provided: ' || p_wheel;
+        END IF;
+    END IF;
+	-- 检查employee是否不为空 且 是否合法，如果空则为null 不合法抛出异常
+    IF p_employee IS NOT NULL AND p_employee <> '' THEN
+        SELECT userName INTO v_employee FROM Employee WHERE username = p_employee;
+        IF v_employee IS NULL THEN
+            RETURN 'Invalid employee provided: ' || p_employee;
+        END IF;
+    END IF;
+   -- 检查description是否不为空，如果空则为null
+   IF p_description IS NOT NULL AND p_description <> '' THEN
+        v_description := p_description;
+   END IF;
+    -- 更新car表
+    UPDATE Car
+    SET 
+        make = p_make,
+        model = p_model,
+        statusID = v_statusID,
+        carTypeID = v_typeID,
+        carWheelID = v_wheelID,
+        purchaseDate = p_purchaseDate,
+        managedBy = v_employee,
+        description = v_description
+    WHERE carID = p_carID;
+
+    RETURN 'Success';
+
+EXCEPTION 
+    WHEN OTHERS THEN
+        RETURN 'An unexpected error occurred: ' || SQLERRM;
 END;
 $$ LANGUAGE plpgsql;
